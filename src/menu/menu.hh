5ea171ea814d9c402ee9aad5d0cef6314008da6a
T0b1 <tasumifox@gmail.com>
2019-10-20 23:46:41 +0200

    fixed fgui i think

--- src/menu/menu.hh
+++ src/menu/menu.hh
@@ -17,25 +17,25 @@ void on_entry_point();
 
 namespace vars {
 
-inline std::unordered_map<std::string, std::shared_ptr<fgui::button>> button;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::checkbox>>
+inline std::unordered_map<std::string, fgui::button*> button;
+inline std::unordered_map<std::string, fgui::checkbox*>
     checkbox;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::colorlist>>
+inline std::unordered_map<std::string, fgui::colorlist*>
     colorlist;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::colorpicker>>
+inline std::unordered_map<std::string, fgui::colorpicker*>
     colorpicker;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::combobox>>
+inline std::unordered_map<std::string, fgui::combobox*>
     combobox;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::container>>
+inline std::unordered_map<std::string, fgui::container*>
     container;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::keybinder>>
+inline std::unordered_map<std::string, fgui::keybinder*>
     keybinder;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::label>> label;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::listbox>> listbox;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::multibox>>
+inline std::unordered_map<std::string, fgui::label*> label;
+inline std::unordered_map<std::string, fgui::listbox*> listbox;
+inline std::unordered_map<std::string, fgui::multibox*>
     multibox;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::slider>> slider;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::spinner>> spinner;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::tabs>> tabs;
-inline std::unordered_map<std::string, std::shared_ptr<fgui::textbox>> textbox;
+inline std::unordered_map<std::string, fgui::slider*> slider;
+inline std::unordered_map<std::string, fgui::spinner*> spinner;
+inline std::unordered_map<std::string, fgui::tabs*> tabs;
+inline std::unordered_map<std::string, fgui::textbox*> textbox;
 } // namespace vars
